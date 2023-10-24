fc5333718cee5377c2e531b256f2a5fd46de7d1c
AysenurKubra <aysenurkubra@gmail.com>
2023-10-24 23:17:08 +0300

    path cards done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+import './App.css';
 import { PathCard } from './component/PathCard';
 
 function App() {
+
+  const [data, setData] = useState([
+    { id: 1, name: 'Visdom', title: 'This path is going through visdom castle', image: '/pictures/visdom.png'},
+    { id: 2, name: 'Courage', title: 'This path is going through courage castle', image: '/pictures/brave.png'},
+    { id: 3, name: 'Love', title: 'This path is going through love castle', image: '/pictures/love.png'}
+  ]);
+
+  const handleClick = () => {
+    return
+  }
+
   return (
-    <div className="App">
-     <PathCard />
+    <div className="container">
+    {
+      data.map(pathCard => 
+      <PathCard 
+        id={pathCard.id} 
+        title={pathCard.title} 
+        name={pathCard.name} 
+        handleClick={handleClick}
+        image={process.env.PUBLIC_URL + pathCard.image} />)
+    }
     </div>
   );
 }
