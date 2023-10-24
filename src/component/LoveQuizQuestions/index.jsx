ea0cd3389fc0a57af34b4ba44d9b31d391c98fc3
AysenurKubra <aysenurkubra@gmail.com>
2023-10-25 01:27:18 +0300

    questions, question card, path done

---
+++ src/component/LoveQuizQuestions/index.jsx
@@ -0,0 +1,64 @@
+export const LoveQuizQuestions = [
+    {
+      id: 1,
+      question: "What is often referred to as the 'language of love'?",
+      options: ["French", "English", "Music", "Math"],
+      correctAnswer: "French"
+    },
+    {
+      id: 2,
+      question: "Who wrote the famous play 'Romeo and Juliet'?",
+      options: ["William Shakespeare", "Jane Austen", "Charles Dickens", "Emily Brontë"],
+      correctAnswer: "William Shakespeare"
+    },
+    {
+      id: 3,
+      question: "What is the ancient Greek word for the different types of love?",
+      options: ["Philia", "Agape", "Eros", "Storge"],
+      correctAnswer: "Philia"
+    },
+    {
+      id: 4,
+      question: "Which of the following is not one of the five love languages?",
+      options: ["Acts of Service", "Physical Touch", "Material Gifts", "Quality Time"],
+      correctAnswer: "Material Gifts"
+    },
+    {
+      id: 5,
+      question: "What is the symbol of love in many cultures?",
+      options: ["Heart", "Rose", "Dove", "Cupid"],
+      correctAnswer: "Heart"
+    },
+    {
+      id: 6,
+      question: "Who is known as the Greek god of love?",
+      options: ["Eros", "Apollo", "Zeus", "Hermes"],
+      correctAnswer: "Eros"
+    },
+    {
+      id: 7,
+      question: "What famous love story involves a prince and a beautiful maiden in India?",
+      options: ["Romeo and Juliet", "Layla and Majnun", "Shah Jahan and Mumtaz Mahal", "Orpheus and Eurydice"],
+      correctAnswer: "Layla and Majnun"
+    },
+    {
+      id: 8,
+      question: "What is the traditional flower associated with love?",
+      options: ["Rose", "Tulip", "Daisy", "Lily"],
+      correctAnswer: "Rose"
+    },
+    {
+      id: 9,
+      question: "What is the title of Jane Austen's novel about love and social class?",
+      options: ["Pride and Prejudice", "Sense and Sensibility", "Emma", "Mansfield Park"],
+      correctAnswer: "Pride and Prejudice"
+    },
+    {
+      id: 10,
+      question: "What is the term for love that is unselfish and selfless?",
+      options: ["Agape", "Eros", "Philia", "Storge"],
+      correctAnswer: "Agape"
+    }
+];
+
+  
---
+++ src/component/WisdomQuizQuestions/index.jsx
@@ -0,0 +1,53 @@
+export const WisdomQuizQuestions = [
+    {
+      question: "What is the definition of wisdom?",
+      options: ["Knowledge", "The ability to make sound decisions", "Intelligence", "Experience"],
+      correctAnswer: "The ability to make sound decisions"
+    },
+    {
+      question: "Who is often associated with wisdom in Greek mythology?",
+      options: ["Zeus", "Athena", "Hercules", "Apollo"],
+      correctAnswer: "Athena"
+    },
+    {
+      question: "Which philosopher is known for his teachings on wisdom and virtue?",
+      options: ["Socrates", "Plato", "Aristotle", "Confucius"],
+      correctAnswer: "Aristotle"
+    },
+    {
+      question: "What is the famous biblical verse about wisdom?",
+      options: ["The fear of the Lord is the beginning of wisdom.", "To be wise is to be free.", "Knowledge is power.", "Wisdom is the key to happiness."],
+      correctAnswer: "The fear of the Lord is the beginning of wisdom."
+    },
+    {
+      question: "Which animal is often used as a symbol of wisdom?",
+      options: ["Owl", "Lion", "Elephant", "Dolphin"],
+      correctAnswer: "Owl"
+    },
+    {
+      question: "In which ancient civilization was the goddess of wisdom, Athena, highly revered?",
+      options: ["Egyptian", "Roman", "Greek", "Mesopotamian"],
+      correctAnswer: "Greek"
+    },
+    {
+      question: "What is the Japanese term for 'wisdom'?",
+      options: ["Seishin", "Chie", "Shichi", "Jinsei"],
+      correctAnswer: "Chie"
+    },
+    {
+      question: "Who wrote the book 'The Art of War,' which contains many insights on strategy and wisdom?",
+      options: ["Confucius", "Sun Tzu", "Laozi", "Machiavelli"],
+      correctAnswer: "Sun Tzu"
+    },
+    {
+      question: "What is the name of the famous wise king in the Bible known for his wisdom in judgment?",
+      options: ["Solomon", "David", "Moses", "Abraham"],
+      correctAnswer: "Solomon"
+    },
+    {
+      question: "Which of the following is not considered one of the three components of wisdom in psychology?",
+      options: ["Cognitive", "Reflective", "Affective", "Proactive"],
+      correctAnswer: "Proactive"
+    }
+  ];
+  
